692fd4b16d6de0e18dc19ef8281129f1ce47c710
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-05-25 22:57:12 -0300

    trying to test

--- test/xtorage_test.js
+++ test/xtorage_test.js
@@ -1,13 +1,16 @@
 "use strict";
 
+const expect = require('chai').expect;
+const Xtorage = new require('../src/xtorage');
+
 describe('xtorage', function() {
     beforeEach(function()
     {
-
+        console.log(window)
     })
 
     it('should return true', function()
     {
-        expect(true).toBe(true);
+        expect(true).to.be.true;
     })
 })
