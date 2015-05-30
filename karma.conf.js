99fc4133c332bbe1e1e031ca84de339b5ca3d3fa
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-05-30 20:08:34 -0300

    pulling down some dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -56,7 +56,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [/*'Chrome', 'IE', 'Opera', 'Safari', 'Firefox', 'ChromeCanary', */'PhantomJS'],
+    browsers: ['Chrome', 'IE', 'Safari', 'Firefox', 'FirefoxNightly', 'ChromeCanary', 'PhantomJS'],
 
 
     // Continuous Integration mode
